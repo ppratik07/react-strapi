aa63ecced6390ab804146a2c20d456fb6f28693d
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-13 10:01:52 +0530

    page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,23 @@
+import FeaturesSection from "@/components/FeaturesSection";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import HowItWorksSection from "@/components/HowItWorksSection";
+import Navbar from "@/components/NavBar";
+import PricingSection from "@/components/PricingSection";
+import { PromoText } from "@/components/PromoText";
+import TestimonialsSection from "@/components/TestimonialsSection";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen bg-white">
+      <Navbar/>
       <HeroSection/>
+      <FeaturesSection/>
+      <HowItWorksSection/>
+      <TestimonialsSection/>
+      <PricingSection/>
+      <PromoText/>
+      <Footer/>
     </main>
   );
 }
